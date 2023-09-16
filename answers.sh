aae11cd51d1613393dc0d6a6426af4e6f1bb64c9
Rayan ALi <rayan.ali.8205@gmail.com>
2023-09-16 19:39:00 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -1,3 +1,4 @@
+ --> Basics Queries 
 1.
  SELECT Name 
  FROM students 
@@ -29,3 +30,30 @@
  UPDATE students
  SET Points=Points-50
  WHERE Name="Alex"
+
+ --> Creating Table 
+
+ CREATE TABLE graduates(
+    graduateID INTEGER NOT NULL PRIMARY KEY,
+    Name TEXT NOT NULL UNIQUE,
+    Age INT,
+    Gender TEXT,
+    Points INT,
+    Graduation TEXT
+)
+
+10.
+ INSERT INTO graduates(Name,Age,Gender,Points)
+ SELECT Name , Age ,Gender,Points
+ FROM students
+ WHERE Name="Layal"
+
+11.
+ UPDATE graduates
+ SET Graduation="08/09/2018"
+ WHERE Name="Layal"
+
+12.
+ DELETE 
+ FROM students
+ WHERE Name="Layal"
